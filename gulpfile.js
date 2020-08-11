c8d87584c4b2b26620c11fcc404a354e4bd54d31
Преображенский Денис <30519612+sailor-den@users.noreply.github.com>
2020-08-11 12:20:10 +0300

    Init

--- gulpfile.js
+++ gulpfile.js
@@ -1,107 +1,169 @@
-var 	gulp         = require('gulp'),
-		sass         = require('gulp-sass'),
-		browserSync  = require('browser-sync'),
-		concat       = require('gulp-concat'),
-		uglify       = require('gulp-uglify-es').default,
-		cleancss     = require('gulp-clean-css'),
-		autoprefixer = require('gulp-autoprefixer'),
-		rsync        = require('gulp-rsync'),
-		newer        = require('gulp-newer'),
-		rename       = require('gulp-rename'),
-		responsive   = require('gulp-responsive'),
-		del          = require('del');
+// VARIABLES & PATHS
 
-// Local Server
-gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
+let preprocessor = 'sass', // Preprocessor (sass, scss, less, styl)
+    fileswatch   = 'html,htm,txt,json,md,woff2', // List of files extensions for watching & hard reload (comma separated)
+    imageswatch  = 'jpg,jpeg,png,webp,svg', // List of images extensions for watching & compression (comma separated)
+    baseDir      = 'app', // Base directory path without «/» at the end
+    distDir      = 'dist',
+    online       = true; // If «false» - Browsersync will work offline without internet connection
+
+let paths = {
+
+	html: {
+		src:  baseDir + "/*.html",
+		dest: distDir + '/',
+	},
+
+	scripts: {
+		src: [
+			// 'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
+			baseDir + '/js/custom.js' // custom.js. Always at the end
+		],
+		dest: distDir + '/js',
+	},
+
+	styles: {
+		src:  baseDir + '/' + preprocessor + '/main.*',
+		dest: distDir + '/css',
+	},
+
+	images: {
+		src:  baseDir + '/images/src/**/*',
+		dest: distDir + '/images',
+	},
+
+	fonts: {
+		src:  baseDir + '/fonts/src/*.ttf',
+		dest: distDir + '/fonts/',
+	},
+
+	deploy: {
+		hostname:    'username@yousite.com', // Deploy hostname
+		destination: 'yousite/public_html/', // Deploy destination
+		include:     [/* '*.htaccess' */], // Included files to deploy
+		exclude:     [ '**/Thumbs.db', '**/*.DS_Store' ], // Excluded files from deploy
+	},
+
+	cssOutputName:   'style.min.css',
+	cssOutputName_2: 'style.css',
+	jsOutputName:    'script.min.js',
+	jsOutputName_2:  'script.js',
+
+}
+
+// LOGIC
+
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass         = require('gulp-sass');
+const scss         = require('gulp-sass');
+const less         = require('gulp-less');
+const styl         = require('gulp-stylus');
+const cleancss     = require('gulp-clean-css');
+const concat       = require('gulp-concat');
+const browserSync  = require('browser-sync').create();
+const uglify       = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin     = require('gulp-imagemin');
+const webp         = require("gulp-webp");
+const newer        = require('gulp-newer');
+const ttf2woff     = require("gulp-ttf2woff");
+const ttf2woff2    = require("gulp-ttf2woff2");
+const rsync        = require('gulp-rsync');
+const del          = require('del');
+
+function browsersync() {
+	browserSync.init({
+		server: { baseDir: distDir + '/' },
 		notify: false,
-		// online: false, // Work offline without internet connection
-		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
+		online: online
 	})
-});
-function bsReload(done) { browserSync.reload(); done(); };
+}
 
-// Custom Styles
-gulp.task('styles', function() {
-	return gulp.src('app/sass/**/*.sass')
-	.pipe(sass({ outputStyle: 'expanded' }))
-	.pipe(concat('styles.min.css'))
-	.pipe(autoprefixer({
-		grid: true,
-		overrideBrowserslist: ['last 10 versions']
-	}))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
-	.pipe(gulp.dest('app/css'))
+function html() {
+    return src(paths.html.src)
+	.pipe(dest(paths.html.dest))
 	.pipe(browserSync.stream())
-});
+}
 
-// Scripts & JS Libraries
-gulp.task('scripts', function() {
-	return gulp.src([
-		// 'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
-		// 'app/js/_lazy.js', // JS library plug-in example
-		'app/js/_custom.js', // Custom scripts. Always at the end
-		])
-	.pipe(concat('scripts.min.js'))
-	.pipe(uglify()) // Minify js (opt.)
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({ stream: true }))
-});
+function scripts() {
+	return src(paths.scripts.src)
+	.pipe(concat(paths.jsOutputName_2))
+	.pipe(dest(paths.scripts.dest))
+	.pipe(concat(paths.jsOutputName))
+	.pipe(uglify())
+	.pipe(dest(paths.scripts.dest))
+	.pipe(browserSync.stream())
+}
 
-// Responsive Images
-gulp.task('img-responsive', async function() {
-	return gulp.src('app/img/_src/**/*.{png,jpg,jpeg,webp,raw}')
-		.pipe(newer('app/img/@1x'))
-		.pipe(responsive({
-			'*': [{
-				// Produce @2x images
-				width: '100%', quality: 90, rename: { prefix: '@2x/', },
-			}, {
-				// Produce @1x images
-				width: '50%', quality: 90, rename: { prefix: '@1x/', }
-			}]
-		})).on('error', function () { console.log('No matching images found') })
-		.pipe(rename(function (path) {path.extname = path.extname.replace('jpeg', 'jpg')}))
-		.pipe(gulp.dest('app/img'))
-});
-gulp.task('img', gulp.series('img-responsive', bsReload));
+function styles() {
+	return src(paths.styles.src)
+	.pipe(eval(preprocessor)())
+	.pipe(concat(paths.cssOutputName_2))
+	.pipe(dest(paths.styles.dest))
+	.pipe(concat(paths.cssOutputName))
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } },/* format: 'beautify' */ }))
+	.pipe(dest(paths.styles.dest))
+	.pipe(browserSync.stream())
+}
 
-// Clean @*x IMG's
-gulp.task('cleanimg', function() {
-	return del(['app/img/@*'], { force: true })
-});
+function images() {
+	return src(paths.images.src)
+	.pipe(newer(paths.images.dest))
+	.pipe(src(paths.images.src))
+	.pipe(
+		webp({
+			quality: 70
+		})
+	)
+	.pipe(dest(paths.images.dest))
+	.pipe(imagemin())
+	.pipe(dest(paths.images.dest))
+}
 
-// Code & Reload
-gulp.task('code', function() {
-	return gulp.src('app/**/*.html')
-	.pipe(browserSync.reload({ stream: true }))
-});
+function fonts() {
+    src(paths.fonts.src)
+        .pipe(ttf2woff())
+        .pipe(dest(paths.fonts.dest))
+    return src(paths.fonts.src)
+       .pipe(ttf2woff2())
+       .pipe(dest(paths.fonts.dest))
+}
 
-// Deploy
-gulp.task('rsync', function() {
-	return gulp.src('app/')
+function cleanimg() {
+	return del('' + paths.images.dest + '/**/*', { force: true })
+}
+
+function deploy() {
+	return src(baseDir + '/')
 	.pipe(rsync({
-		root: 'app/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		// include: ['*.htaccess'], // Included files
-		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files
+		root: baseDir + '/',
+		hostname: paths.deploy.hostname,
+		destination: paths.deploy.destination,
+		include: paths.deploy.include,
+		exclude: paths.deploy.exclude,
 		recursive: true,
 		archive: true,
 		silent: false,
 		compress: true
 	}))
-});
+}
 
-gulp.task('watch', function() {
-	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
-	gulp.watch(['libs/**/*.js', 'app/js/_custom.js'], gulp.parallel('scripts'));
-	gulp.watch('app/*.html', gulp.parallel('code'));
-	gulp.watch('app/img/_src/**/*', gulp.parallel('img'));
-});
+function startwatch() {
+	watch(baseDir  + '/**/*.html', html);
+	watch(baseDir  + '/' + preprocessor + '/**/*', {usePolling: true}, styles);
+	watch(baseDir  + '/images/src/**/*.{' + imageswatch + '}', {usePolling: true}, images);
+	watch(baseDir  + '/**/*.{' + fileswatch + '}', {usePolling: true}).on('change', browserSync.reload);
+	watch([baseDir + '/js/**/*.js', '!' + paths.scripts.dest + '/*.min.js'], {usePolling: true}, scripts);
+}
 
-gulp.task('default', gulp.parallel('img', 'styles', 'scripts', 'browser-sync', 'watch'));
-
+exports.browsersync = browsersync;
+exports.assets      = series(cleanimg, styles, scripts, images);
+exports.html 		= html;
+exports.styles      = styles;
+exports.scripts     = scripts;
+exports.images      = images;
+exports.fonts       = fonts;
+exports.cleanimg    = cleanimg;
+exports.deploy      = deploy;
+exports.default     = parallel(images, styles, scripts, html, browsersync, startwatch);
