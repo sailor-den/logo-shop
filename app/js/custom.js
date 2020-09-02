f02c48b9ceda901d969186ff5cad46f8a603b585
Преображенский Денис <30519612+sailor-den@users.noreply.github.com>
2020-09-02 11:43:06 +0300

    Добавил хедер + адаптив

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,3 +1,11 @@
-function alert () {
-    alert('Test')
-}
+document.addEventListener('DOMContentLoaded', function (){
+
+    document.querySelector('.icon-menu').addEventListener('click', function(){
+        document.querySelector('.menu__body').classList.toggle('menu__body_active')
+    });
+    
+    document.querySelector('.icon-menu').addEventListener('click', function (){
+        this.classList.toggle('icon-menu_active')
+    })
+    
+})
